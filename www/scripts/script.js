5b7323b19fe22466f122d81e296ac6c16eaae27b
GDHub <seunakintoye@hotmail.com>
2017-02-27 11:04:12 +0100

    Addition of Project Files
    Added all necessary files for the Cordova Sample Project

---
+++ www/scripts/script.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    $("#btnRave").click(function (ev) {
+        ev.preventDefault();
+        getpaidSetup({
+            customer_email: localStorage.email,
+            customer_firstname: localStorage.custName,
+            amount: sessionStorage.BookingCost,
+            txref: sessionStorage.BookPayID,
+            PBFPubKey: "FLWPUBK-f7e829bfb6331afb33b122edd4f67aec-X",
+            meta: [{ flightid: 3849 }],
+            onclose: function () { },
+            callback: function (d) { }
+        });
+    });
+});
